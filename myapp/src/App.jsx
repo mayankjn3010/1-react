2e90d6b2217ab3cfffd39b7fa301a6d9bc417697
mayankjn30 <mayankjn3010@gmail.com>
2025-07-16 10:34:58 +0530

    new

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import "./App.scss";
-import "./styles/header.scss";
+import "./styles/header.scsjgs";
 
 function App() {
   return ( 
