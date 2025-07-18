5a1a5ede6f1ad758f00947956aaefbab3ce3595b
mayankjn30 <mayankjn3010@gmail.com>
2025-07-18 12:34:13 +0530

    done

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
-import Services from "./components/services";
+import Services from "./components/Services";
 
 import "./App.scss";
 import "./styles/header.scss";
